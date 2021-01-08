ef2835a2ec39a399edeaae80dc08e2946b944611
sohailshams <sohailshamas@yahoo.com>
2021-01-08 14:57:49 +0000

    Added useEffect function - made request call to get netflixoriginal movies and added function to rendomly select the movie to render

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,6 +4,18 @@ import requests from '../requests';
 
 function Banner() {
   const [movie, setMovie] = useState({});
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
 
   return <header></header>;
 }
