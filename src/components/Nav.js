cb8544f004b96cd52792095931c8f402dd480ed3
sohailshams <sohailshamas@yahoo.com>
2021-01-08 23:07:11 +0000

    Added logo and avatar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import './Nav.css';
-
+import logo from '../images/netflix.png';
 function Nav() {
-  return <div></div>;
+  return (
+    <div className="nav-bar">
+      <img className="nav-logo" src={logo} alt="Netflix Logo" />
+      <img
+        className="nav-avatar"
+        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
 }
 
 export default Nav;
