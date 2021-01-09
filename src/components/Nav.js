942039956ed63bc28d035107df16e42764a2c7fe
sohailshams <sohailshamas@yahoo.com>
2021-01-09 10:52:55 +0000

    Set useState, added useEffect function to trigger when user scroll 100 px down - added nav-black class

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Nav.css';
 import logo from '../images/netflix.png';
 function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener('scroll');
+    };
+  }, []);
   return (
-    <div className="nav-bar">
+    <div className={`nav-bar ${show && 'navbar-black'}`}>
       <img className="nav-logo" src={logo} alt="Netflix Logo" />
       <img
         className="nav-avatar"
